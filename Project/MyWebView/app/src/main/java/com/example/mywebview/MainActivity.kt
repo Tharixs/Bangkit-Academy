b2df2f57ffd03451accb49ec454e69348c804ea1
Tharixs <tharixsakbar@gmail.com>
2023-04-21 02:34:32 +0700

    add annimation starter

--- Project/MyWebView/app/src/main/java/com/example/mywebview/MainActivity.kt
+++ Project/MyWebView/app/src/main/java/com/example/mywebview/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.mywebview
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.JsResult
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import com.example.mywebview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+        binding.webView.settings.javaScriptEnabled = true
+
+        binding.webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                view.loadUrl("javascript:alert('web dicoing berhasil dimuat')")
+            }
+        }
+        binding.webView.webChromeClient = object :  WebChromeClient(){
+            override fun onJsAlert(
+                view: WebView,
+                url: String,
+                message: String,
+                result: JsResult
+            ): Boolean {
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_SHORT).show()
+                result.confirm()
+                return true
+            }
+        }
+        binding.webView.loadUrl("https://www.dicoding.com")
     }
 }
