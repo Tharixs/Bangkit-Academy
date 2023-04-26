6dbc1e8c451ede7cd7356f7b7c28c3cf99f72174
Tharixs <tharixsakbar@gmail.com>
2023-04-26 16:01:32 +0700

    add localization starter project

--- Project/LatihanLocalizationPadaTeksStarter/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
+++ Project/LatihanLocalizationPadaTeksStarter/app/src/main/java/com/dicoding/picodiploma/productdetail/MainActivity.kt
@@ -1,10 +1,10 @@
-package com.example.onboardingpage
+package com.dicoding.picodiploma.productdetail
 
 import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowInsets
 import android.view.WindowManager
-import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +14,11 @@ class MainActivity : AppCompatActivity() {
         setupView()
     }
 
-    private fun setupView(){
+    private fun setupView() {
         @Suppress("DEPRECATION")
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             window.insetsController?.hide(WindowInsets.Type.statusBars())
-        }else{
+        } else {
             window.setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN
