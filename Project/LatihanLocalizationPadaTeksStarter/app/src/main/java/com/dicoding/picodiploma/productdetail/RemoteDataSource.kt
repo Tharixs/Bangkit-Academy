bc36cdc9c26d0fc804dfa44446b060f2757e408f
Tharixs <tharixsakbar@gmail.com>
2023-04-29 14:21:58 +0700

    add myservice app

---
+++ Project/LatihanLocalizationPadaTeksStarter/app/src/main/java/com/dicoding/picodiploma/productdetail/RemoteDataSource.kt
@@ -0,0 +1,24 @@
+package com.dicoding.picodiploma.productdetail
+
+import android.content.Context
+import com.dicoding.picodiploma.productdetail.databinding.ActivityMainBinding
+
+class RemoteDataSource(private val context: Context) {
+
+    fun getDetailProduct(): ProductModel {
+        return ProductModel(
+            name = context.getString(R.string.name),
+            store = context.getString(R.string.store),
+            size = context.getString(R.string.size),
+            color = context.getString(R.string.color),
+            desc = context.getString(R.string.description),
+            image = R.drawable.shoes,
+            date = context.getString(R.string.date),
+            rating = context.getString(R.string.rating),
+            price = context.getString(R.string.price),
+            countRating = context.getString(R.string.countRating)
+        )
+    }
+
+
+}
