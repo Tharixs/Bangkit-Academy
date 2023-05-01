96b17d27716759fc1a04b81ea4f1b3f35d6db49c
Tharixs <tharixsakbar@gmail.com>
2023-05-01 22:57:07 +0700

    add camera x

--- Project/MyPlayer/app/src/main/java/com/example/myplayer/MainActivity.kt
+++ Project/MyPlayer/app/src/main/java/com/example/myplayer/MainActivity.kt
@@ -2,10 +2,99 @@ package com.example.myplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import com.example.myplayer.databinding.ActivityMainBinding
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.util.Util
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+
+    companion object {
+        const val URL_VIDEO_DICODING =
+            "https://github.com/dicodingacademy/assets/releases/download/release-video/VideoDicoding.mp4"
+        const val URL_AUDIO =
+            "https://github.com/dicodingacademy/assets/raw/main/android_intermediate_academy/bensound_ukulele.mp3"
+    }
+
+    private var player: ExoPlayer? = null
+
+    private fun initializePlayer() {
+
+        val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+        val mediaItemAudio = MediaItem.fromUri(URL_AUDIO)
+
+        player = ExoPlayer.Builder(this).build().also { exoPlayer ->
+            binding.videoView.player = exoPlayer
+            exoPlayer.setMediaItem(mediaItem)
+            exoPlayer.addMediaItem(mediaItemAudio)
+            exoPlayer.prepare()
+        }
+    }
+
+    private fun releasePlayer() {
+        player?.release()
+        player = null
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater) // inflate the layout
+        setContentView(binding.root) // set the content view
+
+        val player = ExoPlayer.Builder(this).build()
+        binding.videoView.player = player
+
+        val mediaItem = MediaItem.fromUri(URL_VIDEO_DICODING)
+        val mediaItemAudio = MediaItem.fromUri(URL_AUDIO)
+        player.setMediaItem(mediaItem)
+        player.addMediaItem(mediaItemAudio)
+        player.prepare()
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        hideSystemUi()
+        if (Util.SDK_INT <= 23 || player == null) {
+            initializePlayer()
+        }
+    }
+
+    private fun hideSystemUi() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        WindowInsetsControllerCompat(window, binding.videoView).let { controller ->
+            controller.hide(WindowInsetsCompat.Type.systemBars())
+            controller.systemBarsBehavior =
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (Util.SDK_INT <= 23) {
+            releasePlayer()
+        }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        if (Util.SDK_INT > 23) {
+            releasePlayer()
+        }
     }
 }
